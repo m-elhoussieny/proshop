66320518d9337dde885adcbf48d0e59bde2a78b7
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2020-12-23 20:26:17 +0200

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './component/Header'
+import Footer from './component/Footer'
 
 const App= ()=> {
   return (
     <>
+    <Header />
+    <main className="py-3">
+      <Container>
      <h1>Welcom to Proshop</h1> 
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }
