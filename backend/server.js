f4bf23dba1b93ba09f8e50f9f5a2022ba21d9ab3
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2020-12-31 01:59:29 +0200

    Adding color to the console

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const dotenv=require('dotenv')
+const colors =require('colors')
 const products=require('./data/products')
 const connectDB =require('./config/db')
 
@@ -24,4 +25,4 @@ app.get('/api/products/:id',(req,res)=>{
 
 const PORT =process.env.PORT || 5000
 
-app.listen(PORT,console.log("Server running on port 5000"))
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
