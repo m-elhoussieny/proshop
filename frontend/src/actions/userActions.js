b161c94390e2d1c525d8c336ee5d2947d63485ee
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2021-03-17 20:05:31 +0200

    User Login Screen & Functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST } from "../constans/userConstance"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constans/userConstance"
 
 export const login= (email,password)=> async(dispatch)=>{
     try {
