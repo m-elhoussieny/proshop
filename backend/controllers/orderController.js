ef7c9b698e0976dfce65a024fafa1ad30fecc2ff
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2021-04-04 02:40:28 +0200

    Update To Paid Endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,7 +37,7 @@ const addOrderItems= asyncHandler(async (req,res)=>{
 })
 
 //@desc GET order by id 
-//@route GET/api/orders
+//@route GET/api/orders/:id
 //@access private
 const getOrderById= asyncHandler(async (req,res)=>{
    const order =await Order.findById(req.params.id).populate(
@@ -52,5 +52,29 @@ const getOrderById= asyncHandler(async (req,res)=>{
    }
 })
 
+//@desc UPDATE  order to paid 
+//@route GET/api/orders/:id/pay
+//@access private
+const updateOrderToPaid= asyncHandler(async (req,res)=>{
+    const order =await Order.findById(req.params.id) 
+ 
+    if(order){
+        order.isPaid=true
+        order.paidAt=Date.now()
+        order.paymentResult={
+            id:req.body._id,
+            status:req.body.status,
+            update_time:req.body.update_time,
+            email_address:req.body.payer.email_address
+        }
 
-module.exports={addOrderItems ,getOrderById}
+        const updatedOrder=await order.save()
+        res.json(updatedOrder)
+    }else{
+        res.status(404)
+        throw new Error('Order not found')
+    }
+ })
+
+
+module.exports={addOrderItems ,getOrderById ,updateOrderToPaid}
