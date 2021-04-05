8f3aa3f588665cfdc383551121820c1e449a2b52
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2021-04-05 22:24:09 +0200

    Show Orders On Profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -76,5 +76,13 @@ const updateOrderToPaid= asyncHandler(async (req,res)=>{
     }
  })
 
+ //@desc Get logged in user orders
+//@route GET/api/orders/myorders
+//@access private
+const getMyOrders= asyncHandler(async (req,res)=>{
+    const orders =await Order.find({user : req.user._id}) 
+    res.json(orders)
+ })
 
-module.exports={addOrderItems ,getOrderById ,updateOrderToPaid}
+
+module.exports={addOrderItems ,getOrderById ,getMyOrders ,updateOrderToPaid}
